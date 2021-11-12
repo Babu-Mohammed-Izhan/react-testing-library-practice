7361edb102eac88c0290ea17bbadf0c5135e6760
Izhan <babuizhan1234@gmail.com>
2021-11-12 12:17:07 +0530

    changed test name

--- src/components/Header/__test__/Header.test.js
+++ src/components/Header/__test__/Header.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import Header from "../Header";
 
-test("renders learn react link", () => {
+test("renders text passed into header component", () => {
   render(<Header title="My Header" />);
   const headingElement = screen.getByText(/My Header/i);
   expect(headingElement).toBeInTheDocument();

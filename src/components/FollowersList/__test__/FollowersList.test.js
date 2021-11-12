0e250b5e05c181f7c7caaeaa433d8722759f4efe
Izhan <babuizhan1234@gmail.com>
2021-11-12 22:15:06 +0530

    completed testing for todo app

--- src/components/FollowersList/__test__/FollowersList.test.js
+++ src/components/FollowersList/__test__/FollowersList.test.js
@@ -1,6 +1,75 @@
 import { render, screen } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 import FollowersList from "../FollowersList";
+import axios from "axios";
+jest.mock("axios");
+
+const mockData = {
+  data: {
+    results: [
+      {
+        name: {
+          first: "Mohammed",
+          last: "Izhan",
+        },
+        picture: {
+          large: "https://randomuser.me/api/portraits/men/59.jpg",
+        },
+        login: {
+          username: "Izhan",
+        },
+      },
+      {
+        name: {
+          first: "Mohammed",
+          last: "Izhan",
+        },
+        picture: {
+          large: "https://randomuser.me/api/portraits/men/59.jpg",
+        },
+        login: {
+          username: "Izhan",
+        },
+      },
+      {
+        name: {
+          first: "Mohammed",
+          last: "Izhan",
+        },
+        picture: {
+          large: "https://randomuser.me/api/portraits/men/59.jpg",
+        },
+        login: {
+          username: "Izhan",
+        },
+      },
+      {
+        name: {
+          first: "Mohammed",
+          last: "Izhan",
+        },
+        picture: {
+          large: "https://randomuser.me/api/portraits/men/59.jpg",
+        },
+        login: {
+          username: "Izhan",
+        },
+      },
+      {
+        name: {
+          first: "Mohammed",
+          last: "Izhan",
+        },
+        picture: {
+          large: "https://randomuser.me/api/portraits/men/59.jpg",
+        },
+        login: {
+          username: "Izhan",
+        },
+      },
+    ],
+  },
+};
 
 const MockFollowersList = () => {
   return (
@@ -11,12 +80,14 @@ const MockFollowersList = () => {
 };
 describe("Followers List Functionality", () => {
   test("If one card is rendered", async () => {
+    axios.get.mockResolvedValue(mockData);
     render(<MockFollowersList />);
     const followerdivelement = await screen.findByTestId(`follower-item-0`);
     expect(followerdivelement).toBeInTheDocument();
   });
 
   test("should render multiple cards", async () => {
+    axios.get.mockResolvedValue(mockData);
     render(<MockFollowersList />);
     const followerdivelement = await screen.findAllByTestId(/follower-item-/i);
     expect(followerdivelement.length).toBe(5);
